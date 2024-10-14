9bf46f81694bc0d51e6c8ad8089241113b802841
TaoTao <45528733+huzixuan1@users.noreply.github.com>
2024-10-14 20:31:07 +0800

    Update vectorApi.cpp

--- STL/API/vectorApi.cpp
+++ STL/API/vectorApi.cpp
@@ -99,7 +99,7 @@ void api02()
 
     // assign
     vector<int> v3;
-    v3.assign(v1.begin(),v1.end()-1);
+    v3.assign(v1.begin(),v1.end()-2);
     printvector(v3);
 
     // n个elem方式赋值
@@ -108,12 +108,150 @@ void api02()
     printvector(v4);
 }
 
+void api03()
+{
+    std::vector<int> v1{1,2,3,4,5};
+    printvector(v1);
+
+    if(v1.empty())
+    {
+        std::cout<<"v1 is empty"<<std::endl;
+    }else{
+        std::cout<<"v1 is not empty"<<std::endl;
+        std::cout<<"v1 capacity is:"<<v1.capacity()<<std::endl;
+        std::cout<<"v1 size is:"<<v1.size()<<std::endl;
+    }
+
+    // 重新指定大小
+    v1.resize(15);
+    printvector(v1);
+    std::cout<<"resize is:"<<v1.size()<<std::endl;
+    v1.resize(20,9);
+    printvector(v1);
+    std::cout<<"resize is:"<<v1.size()<<std::endl;
+
+    v1.resize(5);//长于现在的部分全部被delete
+    printvector(v1);
+    std::cout<<"resize is:"<<v1.size()<<std::endl;
+
+
+}
+
+void api04()
+{
+   std::vector<int> v1;
+   v1.push_back(10);
+   v1.push_back(20);
+   v1.push_back(30);
+   v1.push_back(40);
+   v1.push_back(50);
+
+   printvector(v1);
+
+   v1.pop_back();
+   printvector(v1);
+
+   // 头插
+   v1.insert(v1.begin(),100);
+   v1.insert(v1.begin(),3,5);
+   printvector(v1); 
+
+   // 删除
+   v1.erase(v1.begin());
+   printvector(v1);
+
+   // 最后一步
+   // 清空
+   v1.erase(v1.begin(),v1.end());
+   printvector(v1);
+
+}
+
+void api05()
+{
+    std::vector<int> v1;
+    for(int i=0;i<=10;i++)
+    {
+        v1.push_back(i);
+    }
+
+    // 利用[]来获取值
+    for(int i = 0;i<v1.size();i++)
+    {
+        std::cout<<v1[i]<<" ";
+    }
+    std::cout<<std::endl;
+
+    // 利用at来获取数据
+    for(int i = 0;i<v1.size();i++)
+    {
+        std::cout<<v1.at(i)<<" ";
+    }
+
+    std::cout<<std::endl;
+
+    std::cout<<"the first value is:"<<v1.front()<<std::endl;
+    std::cout<<"the last value is:"<<v1.back()<<std::endl;
+}
+
+void api06()
+{
+    // 互换容器
+    std::vector<int> v1{1,2,3,4,5};
+    std::vector<int> v2{6,7,8,9,0};
+
+    v1.swap(v2);
+    printvector(v1);
+    printvector(v2);
+}
+
+
+void api07()
+{
+
+    std::vector<int> v1{1,2,3,4};
+    printvector(v1);
+
+    v1.reserve(10);
+    printvector(v1);
+
+    std::cout<<"size is:"<<v1.capacity()<<std::endl;
+
+    v1.shrink_to_fit();
+    printvector(v1);
+    std::cout<<"size is:"<<v1.capacity()<<std::endl;
+}
+
+void api08()
+{
+    std::vector<int> v1{1,2,3};
+    v1.emplace_back(9);
+    printvector(v1);
+
+    v1.emplace(v1.begin()+2,10);
+    printvector(v1);
+
+}
 
 int main()
 {
-    api01();
+    // api01();
 
-    api02();
+    // api02();
+
+    // api03();
+
+    // api04();
+
+    // api05();
+
+    // api06();
+
+    // api07();
+
+    api08();
+
+
 
     return 0;
 }
