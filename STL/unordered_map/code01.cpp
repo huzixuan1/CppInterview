d34c4c5fa2d3ff671032f725ae8be217b8e66c08
TaoTao <45528733+huzixuan1@users.noreply.github.com>
2024-10-20 15:13:53 +0800

    Update code01.cpp

--- STL/unordered_map/code01.cpp
+++ STL/unordered_map/code01.cpp
@@ -1,1 +1,37 @@
+#include <unordered_map>
+#include <iostream>
+using namespace std;
 
+template<class T,class U>
+void printmap(const std::unordered_map<T,U>& map)
+{
+    for(auto it = map.begin();it!=map.end();it++)
+    {
+        std::cout<<it->first<<":"<<it->second<<std::endl;
+    }
+}
+
+void test01()
+{
+
+    std::unordered_map<int,std::string> map1;
+    map1.emplace(1,"taotao");
+    map1.emplace(2,"ehll");
+    printmap(map1);
+
+    std::cout<<map1.at(2)<<std::endl;
+
+    auto it = map1.find(2);
+    std::cout<<"it is:"<<it->first<<":"<<it->second<<std::endl;
+    
+
+
+}
+
+
+int main()
+{
+    test01();
+
+    return 0;
+}
